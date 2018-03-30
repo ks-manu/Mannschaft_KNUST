2da9902b3b13480c20e49922002fa8381a6d7d7f
Kwaku Manu <kwakumanu@hotmail.com>
2018-03-29 23:56:26 -0400

    Made some modifications to DB with Hassan

--- Back-end/Databases/ClassRepDatabase V 1.0.1.2.sql
+++ Back-end/Databases/ClassRepDatabase V 1.0.1.2.sql
@@ -27,6 +27,9 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `course session` (
+
+--## REPLACE COURSE CODE WITH COURSE NAME WITH CODE IN BRACKETS
+
   `Course_Code` varchar(15) NOT NULL,
   `Starting_Time` time NOT NULL,
   `Ending_Time` time NOT NULL,
@@ -43,14 +46,7 @@ CREATE TABLE IF NOT EXISTS `course session` (
 -- Table structure for table `course table`
 --
 
-CREATE TABLE IF NOT EXISTS `course table` (
-
---## Why not make course tables for each program and assign lecturers to each course?? That way if the same course is being taught by multiple lecturers in different departments, there will be no conflicts
-
-  `Course_Code` varchar(10) NOT NULL,
-  `Course_Name` varchar(15) NOT NULL,
-  PRIMARY KEY (`Course_Code`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+--## REMOVED; ADD COURSE NAME TO SESSION TABLE
 
 -- --------------------------------------------------------
 
@@ -62,15 +58,7 @@ CREATE TABLE IF NOT EXISTS `lecturer table` (
   `Tech_MAil` varchar(20) NOT NULL,
   `First_Name` varchar(10) NOT NULL,
   `Last_Name` varchar(10) NOT NULL,
-  
---## Other_Name should be part of first name
---##  `Other_Name` varchar(15) NOT NULL,
-
   `Password` varchar(20) NOT NULL,
-  
---## Course should be replaced with Department since lecturers teach multiple courses
---##  `Course` varchar(20) NOT NULL,
-
   `Title` varchar(5) NOT NULL,
   PRIMARY KEY (`Tech_MAil`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -83,11 +71,12 @@ CREATE TABLE IF NOT EXISTS `lecturer table` (
 
 CREATE TABLE IF NOT EXISTS `message table` (
 
---## there's no column for the message text
+--## MESSAGE ID TO IDENTIFY MESSAGES. ESPECIALLY VOTABLE ONES
+--## there's no column for the message text, attachment, votability, 
 
   `Sent_By` text NOT NULL,
 
---## suggest to include date in timestamp to differentiate messages
+--## REVIEW TIMESTAMP FORMAT
 
   `Time_sent` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -103,8 +92,7 @@ CREATE TABLE IF NOT EXISTS `students table` (
   `First_Name` varchar(10) NOT NULL,
   `Last_Name` varchar(15) NOT NULL,
 
---## Other_Name should be part of first name
---##  `Other_Names` varchar(15) NOT NULL,
+--## include college
 
   `Programme_Year` varchar(25) NOT NULL,
   `Password` varchar(15) NOT NULL,
@@ -119,8 +107,9 @@ CREATE TABLE IF NOT EXISTS `students table` (
 
 CREATE TABLE IF NOT EXISTS `vote table` (
 
---## there has to be a column for the poll descrption
---## suggest changing `For` AND `Vote_down` to `Up_Vote` and `Down_Vote` respectively
+--## GENERAL VOTE TABLE
+--## there has to be a column for the poll descrption (message id), voters (index number)
+--## suggest changing `For` AND `Vote_down` Vote
 
   `For` varchar(100) NOT NULL,
   `Vote_down` varchar(100) NOT NULL
