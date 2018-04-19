de8ef97882507ee65dbc280704872c516e30d3ec
al-maaz <hassanmaazu@gmail.com>
2018-04-19 18:35:20 +0000

    profile fragment

--- Front-end/Android/app/src/main/java/mannschaft_knust/classrep/ProfileFragment.java
+++ Front-end/Android/app/src/main/java/mannschaft_knust/classrep/ProfileFragment.java
@@ -1,5 +1,6 @@
 package mannschaft_knust.classrep;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
@@ -7,6 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -14,14 +16,26 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View v = inflater.inflate(R.layout.fragment_profile, container, false);
+        View v = inflater.inflate(R.layout.fragment_profile_instructor, container, false);
 
         ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle("Profile");
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setSubtitle("");
 
         return v;
     }
 
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         inflater.inflate(R.menu.menu_profile, menu);
+
+        MenuItem settingsItem = menu.findItem(R.id.action_settings);
+        settingsItem.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                //go to settings activity
+                //Intent intent = new Intent(getContext(),SettingsActivity.class);
+                //startActivity(intent);
+                return false;
+            }
+        });
     }
 }
