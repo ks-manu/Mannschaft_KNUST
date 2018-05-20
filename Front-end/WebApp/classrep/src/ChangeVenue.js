03b83d8cdf2cbeaaadfc011b3ce0e23c183a773a
Cyril Yamoah <ccyamoah@gmail.com>
2018-05-20 00:09:53 +0000

    Added Menubar

--- Front-end/WebApp/classrep/src/ChangeVenue.js
+++ Front-end/WebApp/classrep/src/ChangeVenue.js
@@ -5,6 +5,7 @@ import RaisedButton from 'material-ui/RaisedButton';
 import {Card, CardTitle, CardText} from 'material-ui/Card';
 import Paper from 'material-ui/Paper';
 import paperStyle from './PaperStyle';
+import MenuBar from './MenuBar';
 
 
 
@@ -19,44 +20,32 @@ import paperStyle from './PaperStyle';
        };
        this.handleChange=this.handleChange.bind(this);
      }
-     handleChange(event, index, value) {this.setState({value});}
+     handleChange(event, index, value) {this.setState({value});
+    
+    }
     
     render() {
      return (
-        <div className="container">
+  <div> 
+    <MenuBar/>
     
-    <div>  
-              <Card>
-                <CardTitle title="Change Class Venue"  />
-                
-                <CardText>
-                  <p>Change the location where the class will be held.</p>
-                  <p>There may be another class occupying the class you choose.</p><p>Check from the school's timetable before making your decision.</p>
-                </CardText>
-              </Card>
-            </div>
-        
-    
-            <Paper style={paperStyle}>
-                        <h1>Choose the venue for the class</h1>
-          <div>
-          <DropDownMenu value={this.state.value} onChange={this.handleChange}>
-            <MenuItem value={1} primaryText="Lecture Theatre" />
-            <MenuItem value={2} primaryText="Room A" />
-            <MenuItem value={3} primaryText="Room B" />
-            <MenuItem value={4} primaryText="Levine Hall" />
-            <MenuItem value={3} primaryText="PB" />
-          </DropDownMenu>
-          </div>
-          </Paper>
+    <Paper style={paperStyle}>
+      <h1>Choose the venue for the class</h1>
+      <div>
+        <DropDownMenu value={this.state.value} onClick={this.handleChange}>
+          <MenuItem value={1} primaryText="Lecture Theatre" />
+          <MenuItem value={2} primaryText="Room A" />
+          <MenuItem value={3} primaryText="Room B" />
+          <MenuItem value={4} primaryText="Levine Hall" />
+          <MenuItem value={5} primaryText="PB" />
+        </DropDownMenu>
+        <RaisedButton label="Change Class Venue"/>
+
+      </div>
+    </Paper>
     
     
-          
-    
-          <div>
-           <RaisedButton label="Change Class Venue"/>
-         </div>
-         </div>
+  </div>
       );
      }
     }
