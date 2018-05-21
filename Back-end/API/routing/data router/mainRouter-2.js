ce3fb76f47f0e6b96f0e96bd7b72aa8c58d3d25a
Kwaku Manu <owurakwakumanu@gmail.com>
2018-05-20 20:20:45 -0400

    Resolving integration and consistency issues

--- Back-end/API/routing/data router/mainRouter-2.js
+++ Back-end/API/routing/data router/mainRouter-2.js
@@ -4,7 +4,7 @@ var bodyParser = require('body-parser');
 var authenticator = require("../../authentication/authenticator-2.js");
 var fs = require('fs');
 
-//var authorise = require("../../authentication/authoriser.js");
+var authoriser = require("../../authentication/authoriser.js");
 
 var appRouter = express();
 
@@ -32,18 +32,18 @@ dbConn.connect(function(err) {
 
 //--------------------------------------------POST REQUESTS-----------------------------------------------------------
 //sign in
-appRouter.post('/users/authlib/:user_type/reqID=sign_in', function(request, response){
-    switch(request.params.user_type){
+appRouter.post('/users/authlib/:UserType/reqID=sign_in', function(request, response){
+    switch(request.params.UserType){
         case("Lecturer"):
-            if(!request.body.TechMail || !request.body.Password){
-                var message = "\nFAILURE: No request parameters for Sign In @ " + new Date;
+            if(!request.body.UserID || !request.body.Password){
+                var message = "\nFAILURE: No request parameters on Sign In @ " + new Date;
                 fs.appendFileSync('serverlog', message);
-//ensure both user_id and password are not empty
+//ensure both UserID and password are not empty
                 response.status("400");      //bad request
                 response.send();
             }
             else{
-                var TechMail = request.body.TechMail;
+                var TechMail = request.body.UserID;
                 var Password = request.body.Password;
                 
                 authenticator.lecturerLogin(TechMail, Password, response, dbConn, fs);
@@ -52,7 +52,7 @@ appRouter.post('/users/authlib/:user_type/reqID=sign_in', function(request, resp
             break
         
         case("Student"):
-            if(!request.body.IndexNumber || !request.body.Password){       //ensure both user_id and password are not empty
+            if(!request.body.UserID || !request.body.Password){       //ensure both UserID and password are not empty
                 var message = "\nFAILURE: No request parameters for Sign In @ " + new Date;
                 fs.appendFileSync('serverlog', message);
                 
@@ -60,7 +60,7 @@ appRouter.post('/users/authlib/:user_type/reqID=sign_in', function(request, resp
                 response.send();
             }
             else{
-                var IndexNumber = request.body.IndexNumber;
+                var IndexNumber = request.body.UserID;
                 var Password = request.body.Password;
                 
                 authenticator.studentLogin(IndexNumber, Password, response, dbConn, fs);
@@ -72,9 +72,9 @@ appRouter.post('/users/authlib/:user_type/reqID=sign_in', function(request, resp
 });
 
 //sign out
-appRouter.post('/users/deauthlib/:user_type/reqID=:session_token', function(request, response){
+appRouter.post('/users/deauthlib/:UserType/reqID=:Token', function(request, response){
     //extract token from URL
-    if(!request.params.session_token){
+    if(!request.params.Token){
         var message = "\nFAILURE: No request parameters for Sign Out @ " + new Date;
         fs.appendFileSync('serverlog', message);
         
@@ -82,24 +82,24 @@ appRouter.post('/users/deauthlib/:user_type/reqID=:session_token', function(requ
         response.send();
     }
     else{
-        var session_token = request.params.session_token;
+        var Token = request.params.Token;
         
-        switch(request.params.user_type){
+        switch(request.params.UserType){
             case "Lecturer":
-                authenticator.lecturerLogout(session_token, response, dbConn, fs);
+                authenticator.lecturerLogout(Token, response, dbConn, fs);
                 break
             
             case "Student":
-                authenticator.studentLogout(session_token, response, dbConn, fs);
+                authenticator.studentLogout(Token, response, dbConn, fs);
                 break
         }
     }
 });
 
 ///process posted messages
-appRouter.post('/data/systlab/:session_token/post/:course_table', function(request, response){
+appRouter.post('/data/systlab/:Token/post/:course_table', function(request, response){
     //extract token from URL
-    if(!request.params.session_token){
+    if(!request.params.Token){
         var message = "\nFAILURE: No session token on Post-Store @ "+ new Date;
         fs.appendFileSync('serverlog', message);
         
@@ -107,101 +107,96 @@ appRouter.post('/data/systlab/:session_token/post/:course_table', function(reque
         response.send();
     }
     else{
-    //check contents
-        authoriser.Authorise(request.params.session_token, response, dbConn, fs);       //check session token validity
+        authoriser.Authorise(request.params.Token, response, dbConn, fs);       //check session token validity
         //SQL TIMESTAMP Format 2018-04-22 10:58:16
-        
-        //var session_token = request.params.session_token;
-        //var date = new Date();
-        database.postDb(request.params.session_token, request.params.course_table, request.body.Sent_By, request.body.Time_sent, request.body.Post_ID, request.body.Message, request.body.Votable, request.body.Attachment, dbConn, fs, response);
+        database.postDb(request.params.Token, request.params.CourseCode, request.body.SentBy, request.body.TimeSent, request.body.PostID, request.body.Message, request.body.Votable, request.body.Attachment, dbConn, fs, response);
     }
-    //check session validity    
     
     
 });
 
-//collect results of vote on message
-appRouter.post("/data/systlab/reqID=:session_token/poll", function(request, response){
+//store results of Vote on message
+appRouter.post("/data/systlab/reqID=:Token/poll", function(request, response){
     //extract token from URL
-    if(!request.params.session_token|!request.body.messageID|!request.body.indexNo|!request.body.vote){
+    if(!request.params.Token|!request.body.PostID|!request.body.IndexNumber|!request.body.Vote){
 //        response.set();        //bad request
         response.send();
         fs.appendFileSync('serverlog', '\nFAILURE: POST request on Poll@ '+new Date);        //log activities
 //        console.log("Failed: POST request on Poll");
     }
     else{
-        authoriser.Authorise(request.params.session_token, response, dbConn, fs);
-        database.postVote(request.params.session_token, request.body.messageID, request.body.indexNo, request.body.vote, dbConn, fs, response);
-//        console.log("Success: Token validated for "+session_token+" @ " +date);
+        authoriser.Authorise(request.params.Token, response, dbConn, fs);
+        database.postVote(request.params.Token, request.body.PostID, request.body.IndexNumber, request.body.Vote, dbConn, fs, response);
+//        console.log("Success: Token validated for "+Token+" @ " +date);
     }
 });
 
 //Update course session data
 //Special function only be used by lecturers
-appRouter.post("/data/systlab/course/session/reqID=:session_token", function(request, response){
+appRouter.post("/data/systlab/course/session/reqID=:Token", function(request, response){
     //extract token from URL
-    if(!request.params.session_token | !request.body.alter){
+    if(!request.params.Token | !request.body.alter){
         response.status("400");
-        response.send("Either session_token or alter or both params missing");
+        response.send("Either Token or alter or both params missing");
         fs.appendFileSync('serverlog', '\nFAILURE: Update on Course Sessions @ '+new Date+' #TokenOrAlter');
     }
     else{
-        authoriser.Authorise(request.params.session_token, response, dbConn, fs);       //check session token validity
-        database.updateCourse(request.params.session_token, request.body.alter, request.body.course_code, request.body.starting_time, request.body.ending_time, request.body.techmail, request.body.venue, request.body.programme_year, dbConn, fs, response);
+        authoriser.Authorise(request.params.Token, response, dbConn, fs);       //check session token validity
+        database.updateCourse(request.params.Token, request.body.Alter, request.body.CourseCode, request.body.StartingTime, request.body.EndingTime, request.body.TechMail, request.body.Venue, request.body.ProgrammeAndYear, request.body.Day, dbConn, fs, response);
     }
 });
 
 //---------------------------------------------GET REQUESTS--------------------------------------------------------
 
 //serve requested posts
-appRouter.get('/data/post/reqID=:session_token/:session/:time', function(request, response){
-    if(!request.params.session_token || !request.params.course_code || !request.params.time){
+appRouter.get('/data/post/reqID=:Token/:CourseCode/:time', function(request, response){
+    if(!request.params.Token || !request.params.CourseCode || !request.params.Time){
         fs.appendFileSync('serverlog','\nFAILURE: Request on Get posts @ '+new Date+' #TokenOrCodeOrTime')
         response.status("400");
         response.send('Invalid params');
     }
     else{
-        authoriser.Authorise(request.params.session_token, response, dbConn, fs);       //check session token validity
-        database.getPosts(request.params.session_token, request.params.course_code, request.params.time, response, dbConn, fs);
+        authoriser.Authorise(request.params.Token, response, dbConn, fs);       //check session token validity
+        database.getPosts(request.params.Token, request.params.CourseCode, request.params.Time, response, dbConn, fs);
     }
 });
 
 //Results of poll
-appRouter.get('/data/share/reqID=:session_token/poll/:message_id', function(request, response){
-    if(!request.params.session_token || !request.params.message_id){
+appRouter.get('/data/share/reqID=:Token/poll/:PostID', function(request, response){
+    if(!request.params.Token || !request.params.PostID){
         response.status("400");     //bad request
         response.send("Invalid Details");
         fs.appendFileSync('serverlog', 'FAILURE: GET request on Poll Results @ '+new Date+' #BadParams');
     }
     else{
-        authoriser.Authorise(request.params.session_token, response, dbConn, fs);       //check session token validity
-        database.getPolls(request.params.session_token, request.params.message_id, response, dbConn, fs);
+        authoriser.Authorise(request.params.Token, response, dbConn, fs);       //check session token validity
+        database.getPolls(request.params.Token, request.params.PostID, response, dbConn, fs);
     }
 });
 
 //serve course sessions
-appRouter.get('/data/course/session/reqID=:session_token/share/:user_type/:key', function(request, response){
-    if(!request.params.session_token || !request.params.user_type || !request.params.key){
+appRouter.get('/data/course/session/reqID=:Token/share/:UserType/:Query', function(request, response){
+    if(!request.params.Token || !request.params.UserType || !request.params.Query){
         response.status("400");
         response.send("Invalid Details");
         fs.appendFileSync('serverlog', 'FAILURE: GET request on CCA @ '+new Date+' #BadParams');
     }
     else{
-        authoriser.Authorise(request.params.session_token, response, dbConn, fs);       //check session token validity
-        database.getSessions(request.params.session_token, request.params.user_type, request.params.key, response, dbConn, fs);
+        authoriser.Authorise(request.params.Token, response, dbConn, fs);       //check session token validity
+        database.getSessions(request.params.Token, request.params.UserType, request.params.Query, response, dbConn, fs);
     }
 });
 
 //serve biodata
-appRouter.get('/data/users/share/reqID=:session_token/:user_type/:user_id', function(request, response){
-    if(!request.params.session_token || !request.params.user_type|| !request.params.user_id){
+appRouter.get('/data/users/share/reqID=:Token/:UserType/:UserID', function(request, response){
+    if(!request.params.Token || !request.params.UserType|| !request.params.UserID){
         response.status("400");
         response.send("Invalid Details");
         fs.appendFileSync('serverlog', 'Failed: GET request on Bio Data');
     }
     else{
-        authoriser.Authorise(request.params.session_token, response, dbConn, fs);       //check session token validity
-        database.getBioData(request.params.session_token, request.params.user_type, request.params.user_id, response, dbConn, fs);
+        authoriser.Authorise(request.params.Token, response, dbConn, fs);       //check session token validity
+        database.getBioData(request.params.Token, request.params.UserType, request.params.UserID, response, dbConn, fs);
     }
 });
 
