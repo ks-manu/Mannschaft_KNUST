a5017747bb9a2f065b8d1e89c76c0ed575f42719
jamalissah <issahjamal0@gmail.com>
2018-05-22 11:10:02 +0000

    Made changes to main

--- Front-end/WebApp/classrep/src/Main.js
+++ Front-end/WebApp/classrep/src/Main.js
@@ -8,8 +8,15 @@ import ViewPost from './ViewPost';
 import Timetable from "./Timetable";
 import './App.css';
 import MenuBar from './MenuBar'
+import {Card, CardTitle, CardText,CardMedia} from 'material-ui/Card';
+import GridList from 'material-ui/GridList';
 
-
+var cardStyle = {
+    display: 'block',
+    width: '30vw',
+    transitionDuration: '0.3s',
+    height: '20vw'
+}
 
 class Main extends Component{
   
@@ -25,19 +32,48 @@ class Main extends Component{
       <div>
         <MenuBar/>
         <Paper style={paperStyle} >
-
-          
-          <h1>Welcome to Class Rep</h1>
-          <ul>
-            <Link to="/ViewPost"><li>ViewPost</li></Link>
-            <Link to="/changeVenue"><li>Change Venue</li></Link>
-            <Link to="/changeTime"><li>Change Time</li></Link>
-            
-          </ul>
-          
+          <h1>Welcome to Class Rep</h1> 
         </Paper>
+
+<GridList cols={3} cellHeight={'auto'}>
+      
+        <Card style={cardStyle}>
+           
             
-              
+             <CardText>
+              <a href="/ViewPost"> <CardMedia><img  src="https://images.pexels.com/photos/265076/pexels-photo-265076.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt=""/></CardMedia></a>
+
+             </CardText>
+        <Paper style={paperStyle} >
+          <Link to="/ViewPost"><li><h1>View Posts</h1> </li></Link>
+        </Paper>
+           </Card>
+            
+        
+            
+
+            <Card style={cardStyle}>
+            
+             <CardText>
+<a href="/changeTime"><CardMedia><img  src="https://images.pexels.com/photos/745365/pexels-photo-745365.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt=""/></CardMedia></a>
+             </CardText>
+              <Paper style={paperStyle} >
+          <Link to="/changeTime"><li><h1>Change Time</h1> </li></Link>
+        </Paper>
+           </Card>
+
+
+            <Card style={cardStyle}>
+            
+             <CardText>
+              <a href="/changeVenue"> <CardMedia><img src="https://images.pexels.com/photos/256395/pexels-photo-256395.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt=""/></CardMedia></a>
+             </CardText>
+             <Paper style={paperStyle} >
+          <Link to="/changeVenue"><li><h1>Change Venue</h1> </li></Link>
+        </Paper>
+           </Card>
+            
+      </GridList>
             
       </div>
     );
