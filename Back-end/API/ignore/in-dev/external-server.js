a11652ae24d71462da06c51bf38b35968f87c057
Kwaku Manu <owurakwakumanu@gmail.com>
2018-05-23 16:22:15 +0100

    Package.json & Deleted redundant folders

--- Back-end/API/ignore/in-dev/external-server.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Simple server\n');
-}).listen(8000);
-
-console.log('Server running at port 8000/');
